841846b2fca4c0731d87038bc3f7b324ab8776af
Himeka Tsuchida <yuria@sfc.wide.ad.jp>
2021-09-04 15:29:22 +0900

    適当なタイミングなので

---
+++ LiTty/MemoListView.swift
@@ -0,0 +1,21 @@
+//
+//  MemoListView.swift
+//  LiTty
+//
+//  Created by 土田妃華 on 2021/09/04.
+//
+//
+
+import SwiftUI
+
+struct MemoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MemoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MemoListView()
+    }
+}
